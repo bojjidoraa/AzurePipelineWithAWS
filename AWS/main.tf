bf6b5d7e2229855fa7c820a96b0c9917a6795afa
bojjidoraa <bojjidoraa@gmail.com>
2024-07-26 05:21:04 +0530

    Create main.tf

---
+++ AWS/main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "172.16.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "main"
+  }
+}
+
+#Create security group with firewall rules
+resource "aws_security_group" "jenkins-sg-2022" {
+  name        = var.security_group
+  description = "security group for Ec2 instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = var.security_group
+  }
+}
+
+resource "aws_instance" "myFirstInstance" {
+  ami = "ami-0b8fd93c15b2c81ce"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
+  tags= {
+    Name = "my-ec2-instance"
+  }
+}
+
+# Create Elastic IP address
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "my_elastic_ip"
+  }
+}
