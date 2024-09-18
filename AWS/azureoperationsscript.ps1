fed35e915c2ba6a62362fbc543e0cafce17e061e
bojjidoraa <bojjidoraa@gmail.com>
2024-09-18 17:57:42 +0530

    Update azureoperationsscript.ps1
    added sample scripts

--- AWS/azureoperationsscript.ps1
+++ AWS/azureoperationsscript.ps1
@@ -1,26 +1,4 @@
-[CmdletBinding()]
-param (
-    $OrganizationUrl,
-    $PAT,
-    $OrganizationName
+param(
+[string] $text = "Vinayaka"
 )
-
-$orgUrl = $OrganizationUrl
-$pat = $PAT
-
-# Create header with PAT
-$token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($pat)"))
-$header = @{authorization = "Basic $token"}
-
-
-# Get the list of all projects in the organization
-$projectsUrl = "$OrganizationUrl/_apis/projects?api-version=5.1"
-$projects = Invoke-RestMethod -Uri $projectsUrl -Method Get -ContentType "application/json" -Headers $header
-$projects.value | ForEach-Object {
-   Write-Host $_.id $_.name
-   $user =[pscustomobject]@{
-        'id' = $_.id
-        'name' = $_.name
-    }
-    $user | Export-CSV -Path allprojnameid.csv -Append -NoTypeInformation -Force
-}
+Write-Output $text
